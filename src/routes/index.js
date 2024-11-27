ec1d6a82bb59b0241477e8ed373e6db1c9cf9315
Valérie Song <valerie.song@ormex.io>
2024-11-27 13:02:01 +0100

    création et modification CV

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const AuthenticationRouter = require("./AuthenticationRouter.js");
+const AuthenticationRouter = require("./AuthenticationRouter");
 const UserRouter = require("./UserRouter");
+const CvRouter = require("./CvRouter");
+// const ReviewRouter = require("./ReviewRouter");
 
 const app = express();
 
 app.use("/auth", AuthenticationRouter);
 app.use("/user", UserRouter);
+app.use("/cv", CvRouter);
+// app.use("/review", ReviewRouter);
 
 module.exports = app;
